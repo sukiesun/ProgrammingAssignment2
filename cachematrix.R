e19d51971df0737b5c0100765034f958e81bcc8d
sukiesun <sara.sunsq@gmail.com>
2014-04-27 11:13:03 -0700

    assignment2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,40 @@
-## Put comments here that give an overall description of what your
-## functions do
+## functions makeCacheMatirx and cacheSolve here   
+## are able to cache potentially time-comsuming 
+## computations to calculate inverted matrix
 
-## Write a short comment describing this function
+## makeCacheMatrix will get&set the matrix  
+## and get&set the cache of the inverted matrix 
+## if any
 
 makeCacheMatrix <- function(x = matrix()) {
-
+	m <- NULL        
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }                              ## set the content of the matrix
+        get <- function() x            ## get the content of the matrix
+        setInverse <- function(Inverse) m <<- Inverse                            
+                                       ## set the content of the inverted matrix
+        getInverse <- function() m     ## get the content of the inverted matrix
+        list(set = set, get = get,
+             setInverse = setInverse,
+             getInverse = getInverse)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve will return the cache of the 
+## inverted matrix if any and calculate the 
+## inverse of matrix if no cache
+
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        m <- x$getInverse()            #query the x matrix's cache
+        if(!is.null(m)) {              #if there is a cache
+                message("getting cached data")
+                return(m)              #print message and return cache value, no calculation is required
+        }
+        data <- x$get()                #if there is no cache for the matrix x
+        m <- solve(data, ...)          #calculate the inverted matrix here
+        x$setInverse(m)                #save the value to x's cache
+        m                              #return the result
 }
